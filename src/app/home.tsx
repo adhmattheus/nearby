a12af263a567a0686a2f1ffb8c0d089a7cf10a47
Mattheus Adhonnay <mamenezes@viceri.com.br>
2024-12-16 16:36:55 -0300

    feat: places component

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,11 +1,20 @@
 import { Categories, CategoriesProps } from "@/components/Categories";
+import { PlaceProps } from "@/components/place";
+import { Places } from "@/components/places";
 import { api } from "@/services/api";
 import { useEffect, useState } from "react";
 import { Alert, View } from "react-native";
 
+type MarketsProps = PlaceProps & {
+  latitude: number
+  longitude: number
+}
+
 export default function Home() {
-  const [categories, setCategories] = useState<CategoriesProps>([])
-  const [category, setCategory] = useState("")
+  const [categories, setCategories] = useState<CategoriesProps>([]);
+  const [markets, setMarkets] = useState<MarketsProps[]>([]);
+
+  const [category, setCategory] = useState("");
 
   async function fetchCategories() {
     try {
@@ -18,9 +27,27 @@ export default function Home() {
     }
   }
 
+  async function fetchMarkets() {
+    try {
+      if (!category) {
+        return
+      }
+
+      const { data } = await api.get("/markets/category/" + category)
+      setMarkets(data)
+    } catch (error) {
+      console.log(error)
+      Alert.alert("Locais", "Não foi possível carregar os locais.")
+    }
+  }
+
   useEffect(() => {
     fetchCategories()
-  }, [])
+  }, []);
+
+  useEffect(() => {
+    fetchMarkets()
+  }, [category]);
 
   return (
     <View style={{ flex: 1 }}>
@@ -30,7 +57,7 @@ export default function Home() {
         onSelect={setCategory}
         selected={category}
       />
-
+      <Places data={markets} />
     </View>
   )
 }
